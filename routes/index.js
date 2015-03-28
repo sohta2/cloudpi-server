4b4ccb1fed31f7f0aa8f3f7a15eb646ccda985a8
Sogo Ohta <sohta@yourinventit.com>
2015-03-28 11:10:14 +0900

    Use env to pass irkit variables

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CloudPi Server' });
+  res.render('index', { title: 'CloudPi Server', clientkey: process.env.IRKIT_CLIENT_KEY, deviceid: process.env.IRKIT_DEVICE_ID });
 });
 
 module.exports = router;
