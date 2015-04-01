a701a1c61dc23fe81a9b40071cfb3d03fb42d823
Sogo Ohta <sohta@yourinventit.com>
2015-04-01 20:09:18 +0900

    Change name

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CloudPi Server', clientkey: process.env.IRKIT_CLIENT_KEY, deviceid: process.env.IRKIT_DEVICE_ID });
+  res.render('index', { title: 'My Home Server', clientkey: process.env.IRKIT_CLIENT_KEY, deviceid: process.env.IRKIT_DEVICE_ID });
 });
 
 module.exports = router;
